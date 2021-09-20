c5eec83319911cdb1dbf8a910e1559cc8d0bd08b
mentosmenno2 <mentosmenno2@hotmail.com>
2021-09-20 22:45:53 +0200

    regenerate images after face detection

--- app/Ajax/RemoveFaces.php
+++ app/Ajax/RemoveFaces.php
@@ -4,6 +4,7 @@ namespace Mentosmenno2\ImageCropPositioner\Ajax;
 
 use Mentosmenno2\ImageCropPositioner\Assets;
 use Mentosmenno2\ImageCropPositioner\Helpers\AttachmentMeta;
+use Mentosmenno2\ImageCropPositioner\Regenerate;
 use WP_Error;
 
 class RemoveFaces {
@@ -49,6 +50,17 @@ class RemoveFaces {
 	protected function remove_faces( int $attachment_id ): void {
 		$faces = array();
 		( new AttachmentMeta() )->set_faces( $attachment_id, $faces );
+
+		$regenerated = ( new Regenerate() )->run( $attachment_id );
+		if ( ! $regenerated ) {
+			$error = new WP_Error(
+				500, __( 'Could not regenerate images.', 'image-crop-positioner' ), array(
+					'status' => 500,
+				)
+			);
+			wp_send_json_error( $error, 500 );
+			exit;
+		}
 
 		$data = array(
 			'faces' => $faces,
