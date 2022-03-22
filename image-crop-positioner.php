fee5339ad6ff60432a6a6bc606c578c7431b6558
Menno van den Ende <50165380+menno-ll@users.noreply.github.com>
2022-03-22 12:00:53 +0100

    Update image-crop-positioner.php

--- image-crop-positioner.php
+++ image-crop-positioner.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name:       Image Crop Positioner
- * Version:           0.2.1
+ * Version:           0.2.2
  * Description:       Face recognition and hotspot selection for cropping images in WordPress.
  * Plugin URI:        https://github.com/mentosmenno2/image-crop-positioner
  * Author:            mentosmenno2
